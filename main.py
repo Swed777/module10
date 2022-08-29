5ec6783531927babb070da3f15651d21aaa63b67
Swed777 <107791675+Swed777@users.noreply.github.com>
2022-08-29 13:42:06 +0000

    3_4_6_finish

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 #Зайдите в файлы с заданиями и выполните так чтобы вывод соотвествовал условиям.
 #import task_1
 #import task_2
-import task_3
+#import task_3
 import task_4
 import task_5
 import task_6
--- task_6.py
+++ task_6.py
@@ -4,5 +4,18 @@ print('--------------------------')
 # которая запрашивает у пользователя число N
 # и находит сумму факториалов 1! + 2! + 3! +... + N! 
 
+fact = 1
+summ_fact = 0
+
+number = int(input('Введите число для вычисления суммы факториалов: '))
+
+for i_number in range(1, number + 1):
+    for f_act in range(1, i_number + 1):
+        fact *= f_act
+    summ_fact += fact
+    fact = 1
+
+print('Сумма факториалов числа ', number, ': =', summ_fact)
+
 
 print('--------------------------')
